12abd8899025b892ce16e467b83cdc63b36f1c7d
mimmeer <175632698+mimmeer-github@users.noreply.github.com>
2024-09-21 19:16:07 +0100

    Update trap.js

--- src/trap.js
+++ src/trap.js
@@ -1,7 +1,7 @@
 // Function to disable keys
 function disableKeys(e) {
     // Check for Ctrl+Shift+I, Ctrl+Shift+C, F11, and F12
-    if ((e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'i' || e.key === 'C' || e.key === 'c')) ||
+    if ((e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'i' || e.key === '' || e.key === 'c')) ||
         e.key === 'F12') {
         e.preventDefault();
         return false;
